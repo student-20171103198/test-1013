9cc09dc2a78c7751c1dac2f36646c7016cc16deb
student-20171103198 <744136379@qq.com>
2018-10-13 10:05:24 +0800

    Caculator
    计算器

--- test-1013/ViewController.swift
+++ test-1013/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    
+    @IBOutlet weak var y: UITextField!
+    
+    @IBOutlet weak var z: UITextField!
+    
+    @IBAction func caculator(_ sender: Any) {
+        var ciecx = 0
+            ciecx = Int(x.text!)!
+        var ciecy = 0
+            ciecy = Int(y.text!)!
+        var ciecz:Int = 0
+            ciecz = ciecx + ciecy
+        z.text = "\(ciecz)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        x.text = "0"
+        y.text = "0"
+        z.text = "0"
     }
 
 
